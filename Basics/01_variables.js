2776797c24455fa302215f8d60fd9a9462e1e6bb
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2025-09-22 07:38:56 +0000

    conversion of Datatypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code as newer version
+
+// let number="32ac";
+// console.log(Number(number)); //number mein convert nahi hoga it show in output NaN.
+// console.log(typeof(number));
+// console.log(number);
+
+
+let checkBool="shivam";
+let converseBool=Boolean(checkBool);
+
+console.log(converseBool);
